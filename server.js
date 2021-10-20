ea5b59329ae267304b0e28d5c7eadc38cdab7190
Emily Dorgan <wmdorgan@gmail.com>
2021-10-19 20:35:38 -0700

    set up the middlwares and the /api/notes path

--- server.js
+++ server.js
@@ -9,13 +9,26 @@ const PORT = 3001;
 
 const app = express();
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.use(express.static('public'));
+
+// use encoding and express.json so that the express.js can actually read json notation
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// User gets sent to landing page (index.html) (note: change to * GET command later)
+
+app.get('/api/notes', (req, res) =>{
+    res.json(db)
+    }
 );
 
-// User gets sent the notes.html
+// User gets sent the notes.html page upon request
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
