ff77f45218faa8171dfca59007f9be016584669c
Emily Dorgan <wmdorgan@gmail.com>
2021-10-19 21:15:38 -0700

    set up the post request, confirmed that it is going through, installed uuidv4 to generate unique IDs for saved notes

--- server.js
+++ server.js
@@ -3,20 +3,45 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json')
+const {v4 : uuidv4} = require('uuid')
 
+const notesList = [];
 
+// Port defined and express.js initialized
 const PORT = 3001;
-
 const app = express();
 
+// Middleware to (respectively) read the public directory's assets (CSS and JS), encode data and allow express to read json
 app.use(express.static('public'));
-
-// use encoding and express.json so that the express.js can actually read json notation
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// User gets sent to landing page (index.html) (note: change to * GET command later)
+app.post('/api/notes', (req, res) =>{
+    console.log(`${req.method} request recieved to log note`)
 
+    const {title, text} = req.body;
+
+    if(title && text){
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+        notesList.push(newNote);
+
+        const response = {
+            status: "success",
+            body: newNote,
+        };
+
+        console.log(response);
+        res.json(response);
+    } else{
+        res.json("failed to save note");
+    }
+});
+
+// User gets sent to landing page (index.html) (note: change to * GET command later)
 app.get('/api/notes', (req, res) =>{
     res.json(db)
     }
@@ -27,10 +52,12 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Root, user gets sent to index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// set up the host port
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
